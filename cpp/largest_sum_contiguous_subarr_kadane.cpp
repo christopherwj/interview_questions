a42f889ff5075182746608ff67d8e3be92f3c21e
christopherwj <38334130+christopherwj@users.noreply.github.com>
2022-05-17 20:47:57 -0700

    Update largest_sum_contiguous_subarr_kadane.cpp

--- cpp/largest_sum_contiguous_subarr_kadane.cpp
+++ cpp/largest_sum_contiguous_subarr_kadane.cpp
@@ -6,18 +6,30 @@ using namespace std;
  
 int maxSubArraySum(int a[], int size)
 {
-    int max_so_far = INT_MIN, max_ending_here = 0;
+    int max_so_far = INT_MIN, max_ending_here = 0,
+       start =0, end = 0, s=0;
  
-    for (int i = 0; i < size; i++)
+    for (int i=0; i< size; i++ )
     {
-        max_ending_here = max_ending_here + a[i];
+        max_ending_here += a[i];
+ 
         if (max_so_far < max_ending_here)
+        {
             max_so_far = max_ending_here;
+            start = s;
+            end = i;
+        }
  
         if (max_ending_here < 0)
+        {
             max_ending_here = 0;
+            s = i + 1;
+        }
     }
-    return max_so_far;
+    cout << "Maximum contiguous sum is "
+        << max_so_far << endl;
+    cout << "Starting index "<< start
+        << endl << "Ending index "<< end << endl;
 }
  
 /*Driver program to test maxSubArraySum*/
@@ -26,6 +38,13 @@ int main()
     int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int n = sizeof(a)/sizeof(a[0]);
     int max_sum = maxSubArraySum(a, n);
-    cout << "Maximum contiguous sum is " << max_sum;
     return 0;
 }
+
+/*
+Kadane’s Algorithm can be viewed both as a greedy and DP. As we can see that we are keeping a running sum of integers and when it becomes less than 0, we reset it to 0 (Greedy Part). This is because continuing with a negative sum is way more worse than restarting with a new range. Now it can also be viewed as a DP, at each stage we have 2 choices: Either take the current element and continue with previous sum OR restart a new range. These both choices are being taken care of in the implementation. 
+
+Time Complexity: O(n)
+
+Auxiliary Space: O(1)
+*/
