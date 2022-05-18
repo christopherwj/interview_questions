7f1d0b5ba3f98e74758a5342b1cdc9bf92105e96
christopherwj <38334130+christopherwj@users.noreply.github.com>
2022-05-17 20:04:10 -0700

    Update 2_dim_array_malloc.c

--- c/2_dim_array_malloc.c
+++ c/2_dim_array_malloc.c
@@ -1,6 +1,7 @@
 /*
 Write a function called my2DAlloc which allocates a two dimensional array.
 Minimize the number of calls to malloc and make sure that the memory is accessible by the notation arr[i][j].
+https://tianrunhe.wordpress.com/2012/04/23/aligned-malloc-in-c/
 */
 
 int** My2DAlloc(int rows, int cols) {
