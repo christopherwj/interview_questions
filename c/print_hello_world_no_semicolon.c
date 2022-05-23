8bb36deb5f8e835772504de6d2d4e2aac54d33c0
christopherwj <38334130+christopherwj@users.noreply.github.com>
2022-05-23 16:27:21 -0700

    Create print_hello_world_no_semicolon.c

---
+++ c/print_hello_world_no_semicolon.c
@@ -0,0 +1,20 @@
+#include<stdio.h>  
+ int main()    
+{    
+ if(printf("hello world")){}    
+return 0;  
+}   
+
+#include<stdio.h>  
+ int main()    
+{    
+switch(printf("hello world")){}    
+return 0;  
+}  
+
+#include<stdio.h>  
+ int main()    
+{    
+   while(!printf("hello world")){}    
+return 0;  
+}  
